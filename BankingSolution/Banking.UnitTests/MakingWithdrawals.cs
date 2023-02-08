76761d00474f81e2cb82f45707cab2064cc75565
CheBap <Che_Baptiste@progressive.com>
2023-02-08 12:33:05 -0500

    myChanges - need to get Jeff's code

---
+++ BankingSolution/Banking.UnitTests/MakingWithdrawals.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking.UnitTests;
+
+public class MakingWithdrawals
+{
+    [Fact]
+    public void MakingAWithdrawalDecreaseBalance()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToWithdraw = 100M;
+
+        account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
+    }
+
+}
