6a40f1fd09452df27332055a729e1fef0b243181
CheBap <Che_Baptiste@progressive.com>
2023-02-08 14:22:41 -0500

    none and single

---
+++ practices/stringcalculator-wednesday1/stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Single()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("1");
+
+        Assert.Equal(1, result);
+    }
+}
