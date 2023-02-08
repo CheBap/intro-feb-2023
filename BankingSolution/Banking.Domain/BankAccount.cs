50c851ead5e9600e84afe2b88020407c9775f9ad
CheBap <Che_Baptiste@progressive.com>
2023-02-08 13:50:40 -0500

    proper code

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -2,7 +2,7 @@
 
 public class BankAccount
 {
-    private decimal _balance = 5000M;
+    private decimal _balance = 5000M; // State - "Fields" variable.
     public void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
@@ -15,6 +15,21 @@ public class BankAccount
 
     public void Withdraw(decimal amountToWithdraw)
     {
-        _balance -= amountToWithdraw;
+        if (NotOverdraft(amountToWithdraw))
+        {
+            _balance -= amountToWithdraw;
+        }
+        else
+        {
+            throw new AccountOverdraftException();
+        }
+
+    }
+
+
+    // "Never type private, always refactor to it" - Corey Haines.
+    private bool NotOverdraft(decimal amountToWithdraw)
+    {
+        return _balance >= amountToWithdraw;
     }
 }
--- BankingSolution/Banking.UnitTests/MakingWithdrawals.cs
+++ BankingSolution/Banking.UnitTests/MakingWithdrawals.cs
@@ -1,17 +1,56 @@
 ﻿namespace Banking.UnitTests;
-
 public class MakingWithdrawals
 {
-    [Fact]
-    public void MakingAWithdrawalDecreaseBalance()
+    private BankAccount _account;
+    private decimal _openingBalance;
+
+    public MakingWithdrawals()
     {
-        var account = new BankAccount();
-        var openingBalance = account.GetBalance();
-        var amountToWithdraw = 100M;
-
-        account.Withdraw(amountToWithdraw);
-
-        Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
+        _account = new BankAccount();
+        _openingBalance = _account.GetBalance();
     }
 
+    [Theory]
+    [InlineData(100)]
+    [InlineData(183.23)]
+    public void MakingAWithdrawalDecreasesBalance(decimal amountToWithdraw)
+    {
+
+        _account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(_openingBalance - amountToWithdraw, _account.GetBalance());
+    }
+
+    [Fact]
+    public void OverdraftIsNotAllowedBalanceStaysTheSame()
+    {
+        var amountToWithdraw = _openingBalance + .01M;
+        try
+        {
+            _account.Withdraw(amountToWithdraw); // sus!
+        }
+        catch (AccountOverdraftException)
+        {
+            // was expecting that... carry on. 
+        }
+        Assert.Equal(_openingBalance, _account.GetBalance());
+
+    }
+
+    [Fact]
+    public void OverdraftThrowsException()
+    {
+        Assert.Throws<AccountOverdraftException>(() =>
+             _account.Withdraw(_openingBalance + .01M)
+             );
+
+    }
+
+
+    [Fact]
+    public void CanTakeEntireBalance()
+    {
+        _account.Withdraw(_account.GetBalance());
+        Assert.Equal(0, _account.GetBalance());
+    }
 }
