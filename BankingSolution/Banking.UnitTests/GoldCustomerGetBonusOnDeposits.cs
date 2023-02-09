10cccea683062886440aa6c62cb224e01fa6e197
CheBap <Che_Baptiste@progressive.com>
2023-02-09 11:47:15 -0500

    goldcustomergetbonusondeposits

--- BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Banking.UnitTests;
 
-namespace Banking.UnitTests
+
+public class GoldCustomerGetBonusOnDeposits
 {
-    internal class GoldCustomerGetBonusOnDeposits
+    [Fact]
+    public void BonusAppliedToDeposit()
     {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+
     }
+    
+    
 }
