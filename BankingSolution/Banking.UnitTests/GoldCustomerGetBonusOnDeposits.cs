2e4e3cdc287698dad5bb383b45c7efa6b9e80227
CheBap <Che_Baptiste@progressive.com>
2023-02-09 12:33:58 -0500

    given when then of gold

--- BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
@@ -7,12 +7,15 @@ public class GoldCustomerGetBonusOnDeposits
     [Fact]
     public void BonusAppliedToDeposit()
     {
+        //given
         var account = new BankAccount(new DummyBonusCalculator());
         var openingBalance = account.GetBalance();
         var amountToDeposit = 100M;
 
+        //when
         account.Deposit(amountToDeposit);
 
+        //then
         Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
 
     }
