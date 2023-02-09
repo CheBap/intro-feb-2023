bae72b21d0ffd8e2f3373b676c0bfe97d843148d
CheBap <Che_Baptiste@progressive.com>
2023-02-09 11:44:36 -0500

    inheritance

--- BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerGetBonusOnDeposits.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Banking.UnitTests;
 
-namespace Banking.UnitTests
+public class GoldCustomerGetBonusOnDeposits
 {
-    internal class GoldCustomerGetBonusOnDeposits
+    [Fact]
+    public void BonusAppliedToDeposit()
     {
+        var account = new GoldBankAccount();
+        var openingBalance = account.GetBalance();
+        //extra code here
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal (openingBalance + amountToDeposit + 10M, account.GetBalance());
     }
 }
