2d98dfd6873ffc31a7bb11c8be2f28d963dd579b
CheBap <Che_Baptiste@progressive.com>
2023-02-10 16:09:38 -0500

    woo api

--- practices/stringcalculator-part2/StringCalculator.cs
+++ practices/stringcalculator-part2/StringCalculator.cs
@@ -3,9 +3,61 @@ namespace StringCalculator;
 
 public class StringCalculator
 {
+    private ILogger _logger;
+    private IWebService _webService; //can add iwebservice to logger by doing ctrl+.
+
+    public StringCalculator(ILogger logger, IWebService webService)
+    {
+        _logger = logger;
+        _webService = webService;
+    }
 
     public int Add(string numbers)
     {
-        return -42;
+        int answer = 0;
+        if (numbers == "")
+        {
+
+            answer = 0;
+        }
+        else
+        {
+            answer = numbers.Split(',', '\n')
+                .Select(int.Parse)
+                .Sum();
+        }
+        // WTCYWYH
+        try //ctrl k + ctrl s to generate try statement
+        {
+            _logger.Write(answer.ToString());
+        }
+        catch (LoggerException ex)
+        {
+            //write the code you wish you had
+            _webService.NotifyOfFailedLogging(ex.Message);
+        }
+
+
+        return answer;
+    }
+}
+
+public interface ILogger
+{
+    void Write(string message);
+}
+
+public interface IWebService
+{
+    void NotifyOfFailedLogging(string message);
+}
+
+
+public class LoggerException : ApplicationException
+{
+    public string Message { get; private set; } = ""; //you can get this but can only set it in here
+    public LoggerException(string message)
+    {
+        Message = message;
     }
 }
---
+++ practices/stringcalculator-part2/StringCalculatorInteractionTests.cs
@@ -0,0 +1,47 @@
+﻿using Castle.Core.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator;
+
+public class StringCalculatorInteractionTests
+{
+
+    [Theory]
+    [InlineData("1,2", "3")]
+    [InlineData("1,2,3,4,5,6,7,8,9", "45")]
+    public void AnswersAreLoggedToLogger(string numbers, string expected)
+    {
+        // Given
+        var mockedLogger = new Mock<ILogger>();
+        var calculator = new StringCalculator(mockedLogger.Object, new Mock<IWebService>().Object); // NEED SOMETHING HERE
+
+        // When
+        calculator.Add(numbers);
+
+        // Then
+        // I need to verify (assert) that the logger was called with the expected value!
+        mockedLogger.Verify(logger => logger.Write(expected), Times.Once);
+    }
+
+    [Fact]
+    public void WhenLoggerBlowsUpTheWebServiceIsCalled()
+    {
+        //given
+        var stubbedLogger = new Mock<ILogger>();
+        stubbedLogger.Setup(m=>m.Write(It.IsAny<string>())).Throws(new LoggerException("Blammo"));
+        var mockedWebService = new Mock<IWebService>();
+        var calculator = new StringCalculator(stubbedLogger.Object, mockedWebService.Object);
+
+        //when
+        calculator.Add("1");
+
+        //then
+        //?? the web sersvice should be called
+        mockedWebService.Verify(ws =>ws.NotifyOfFailedLogging("Blammo"));
+    }
+}
